28bfdfe9d92900a0c73782095e913baf125aea5b
Yongho Kim <ironyonghokim@gmail.com>
2022-09-19 22:41:08 -0500

    Added config; Improved user experience

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 build:
+	go build -o ./out/jetson-exporter jetson_exporter.go
+
+build-arm64:
 	GOOS=linux GOARCH=arm64 go build -o ./out/jetson-exporter jetson_exporter.go
+
--- jetson_exporter.go
+++ jetson_exporter.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
@@ -11,16 +14,34 @@ import (
 	"github.com/waggle-sensor/jetson-exporter/collector"
 )
 
-// loadPath                = "/tmp/load"
-// currentDeviceFrqPathRex = "/sys/devices/gpu.0/devfreq/*/cur_freq"
-// loadPath                = "/sys/devices/gpu.0/load"
-// currentDeviceFrqPathRex = "/sys/devices/gpu.0/devfreq/*/cur_freq"
+func getenv(key string, def string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return def
+}
 
 func main() {
+	var (
+		nodeName    string
+		metricsPath string
+	)
+	flag.StringVar(&nodeName, "nodename", getenv("KUBENODE", ""), "Sampling interval in milliseconds")
 	config := &collector.TegraGPUCollectorConfig{}
 	flag.IntVar(&config.CollectionIntervalInMilli, "sampling", 100, "Sampling interval in milliseconds")
 	flag.StringVar(&config.LoadPath, "loadpath", "/sys/devices/gpu.0/load", "Path to GPU load")
 	flag.StringVar(&config.CurrentDeviceFrqPathRex, "devfreqpathrex", "/sys/devices/gpu.0/devfreq/*/cur_freq", "Path described in Regression to current frequency of GPU device")
+	if nodeName != "" {
+		metricsPath = path.Join("/", nodeName, "/metrics")
+	} else {
+		metricsPath = "/metrics"
+	}
+
+	fmt.Printf("Jetson exporter started\n")
+	fmt.Println("Parameters are:")
+	fmt.Printf("\t Sampling Interval: %d millisecond\n", config.CollectionIntervalInMilli)
+	fmt.Printf("\t Loadpath: %s\n", config.LoadPath)
+	fmt.Printf("\t Endpoint: %s\n", metricsPath)
 	collector := collector.NewTegraGPUCollector(config)
 	collector.Configure()
 	stopCh := make(chan bool, 1)
@@ -28,6 +49,6 @@ func main() {
 	reg.MustRegister(collectors.NewGoCollector())
 	reg.MustRegister(collector)
 	go collector.RunUntil(stopCh)
-	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true}))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.Handle(metricsPath, promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true}))
+	log.Fatal(http.ListenAndServe("0.0.0.0:9100", nil))
 }
